2584627292cf359134c3d3b86dc4bad6091048ed
ozank <ozikeysan@gmail.com>
2025-09-16 14:52:30 +0100

    10 MW optimization

--- pre_optimization.m
+++ pre_optimization.m
@@ -47,7 +47,7 @@ bounds = [15 30;      % Number of Poles ( the value divided by 4) due to simulat
           30 200;     % 5- stator.N_turns
           0.35 0.45;   % 6- stator.coil_width_to_coil_pitch_ratio
           0.01 0.05;      % 7- stator.coil_thickness    
-          3  3]      %8- machine.Nstacks
+          5  5]      %8- machine.Nstacks
 
 % Lower Bounds for optimization inputs
 lower_bounds = bounds(:,1)   % First column assigned to lower bounds
