8e7de3b876b2fafb27c53df2a26af0a33a4ec433
ozank <ozikeysan@gmail.com>
2025-10-23 11:57:00 +0100

    presentation plots

--- biot_savart_template.m
+++ biot_savart_template.m
@@ -17,7 +17,7 @@ dGamma2 = 1e-2; % filament max discretization step [m], default to 1 cm
 
 %% Get Machine Parameters
 machine_parameters;      %large machine parameters
-small_machine_parameters;      %large machine parameters
+%small_machine_parameters;      %large machine parameters
 
 %% Get Material Properties
 material_constants; % Load material constants
@@ -44,7 +44,7 @@ material_constants; % Load material constants
 % comment out below for race track coil
 
  axial_winding_coordinates;
- Npoles_radial = 8; % Number of modules to be simulated in the radial direction, default 8
+ Npoles_radial = 4; % Number of modules to be simulated in the radial direction, default 8
  %Npoles_radial = machine.Npole/4; 
 
 % Add windings for the axial race track winding
@@ -59,40 +59,40 @@ material_constants; % Load material constants
 %% Biot Savart Calculation Space Definitions
 % Field points (where we want to calculate the field)
 
-% %% Solution Space A: Polar Plane Segment on Z-axis
-% % %Solution Space 
- R_min = 0.08;  %Solution space inner radius
- R_max = 0.28; %Solution space outer space
-% 
- angle_offset = 0; %Solution space starting point
- angle_span = 360; % Solution angle span (degrees)
-% 
- data_point_angle= 360;  % number of data points in the tangential directions (through angle)
- data_point_radius = 50; %number of data points in the radial (radius) direction
-% 
- r_M = linspace (R_min,R_max, data_point_radius+1);
- angle_M = linspace (angle_offset,angle_offset + angle_span, data_point_angle+1);
-% 
-% %create polar coordinate points
- [R_M,ANGLE_M] = ndgrid(r_M,angle_M * (pi()/180));
-% 
-% %Convert from polar coordinates to cartesian points
- [X_M,Y_M] = pol2cart(ANGLE_M,R_M);
- Z_M = zeros(data_point_radius+1,data_point_angle+1); % z [m] 
+% % %% Solution Space A: Polar Plane Segment on Z-axis
+% % % %Solution Space 
+%  R_min = 0.15;  %Solution space inner radius
+%  R_max = 0.32; %Solution space outer space
+% % 
+%  angle_offset = 0; %Solution space starting point
+%  angle_span = 360; % Solution angle span (degrees)
+% % 
+%  data_point_angle= 3600;  % number of data points in the tangential directions (through angle)
+%  data_point_radius = 50; %number of data points in the radial (radius) direction
+% % 
+%  r_M = linspace (R_min,R_max, data_point_radius+1);
+%  angle_M = linspace (angle_offset,angle_offset + angle_span, data_point_angle+1);
+% % 
+% % %create polar coordinate points
+%  [R_M,ANGLE_M] = ndgrid(r_M,angle_M * (pi()/180));
+% % 
+% % %Convert from polar coordinates to cartesian points
+%  [X_M,Y_M] = pol2cart(ANGLE_M,R_M);
+%  Z_M = zeros(data_point_radius+1,data_point_angle+1); % z [m] 
 
 % Solution SPace B: Cylindrical Surface at a Specific Radius
 
 %Solution Space 
 R = HTS.R_mean;  % Radius of the cylindrical surface
 
-Z_min = -0.4;  %Solution space minimum Z point
-Z_max = 0.4; %Solution space maximum Z point
+Z_min = -0.08;  %Solution space minimum Z point
+Z_max = 0.08; %Solution space maximum Z point
 
-angle_offset = 6; %Solution space starting point
-angle_span = 12; % Solution angle span (degrees)
+angle_offset = 0; %Solution space starting point
+angle_span = 45; % Solution angle span (degrees)
 
-data_point_angle= 25;  % number of data points in the tangential directions (through angle)
-data_point_radius = 50; %number of data points in the Z direction
+data_point_angle= 40;  % number of data points in the tangential directions (through angle)
+data_point_radius = 20; %number of data points in the Z direction
 
 r_M = linspace (R, R, data_point_radius+1);  %Constant R values
 angle_M = linspace (angle_offset,angle_offset + angle_span, data_point_angle+1);
@@ -107,11 +107,11 @@ z_M = linspace (Z_min,Z_max, data_point_radius+1);
 Z_M = repmat(z_M,  data_point_angle+1,1)';
 
 % BIOT SAVART ANALYSIS RUN
-Biot-Savart Integration
+%Biot-Savart Integration
  [BSmag,X,Y,Z,BX,BY,BZ] = BSmag_get_B(BSmag,X_M,Y_M,Z_M);
 
 %% Post Processing 
-BSmag_plot_field_points(BSmag,X_M,Y_M,Z_M); % -> shows the field point line
+%BSmag_plot_field_points(BSmag,X_M,Y_M,Z_M); % -> shows the field point line
 
 % Plot B/|B|
 figure(1)
@@ -125,7 +125,7 @@ figure(1)
 xlabel ('x [m]'), ylabel ('y [m]'), title ('Bz [T]')
 % 
 
-Plot Bz on the plane
+%Plot Bz on the plane
 figure(2), hold on, box on, grid on
     contourf(R_M.*sin(ANGLE_M), Z, sqrt(BX.^2+BY.^2+BZ.^2)), colorbar
 xlabel ('x [m]'), ylabel ('y [m]'), title ('Bmag [T]')
@@ -139,15 +139,15 @@ xlabel ('x [m]'), ylabel ('y [m]'), title ('B_tangential [T]')
 
 
 % Plot Bz on the plane
-figure(2), hold on, box on, grid on
-    contourf(X, Y, BZ), colorbar
-xlabel ('x [m]'), ylabel ('y [m]'), title ('Bz [T]')
+% figure(2), hold on, box on, grid on
+%     contourf(X, Y, BZ), colorbar
+% xlabel ('x [m]'), ylabel ('y [m]'), title ('Bz [T]')
 
 % %Other Plot Options for Reserve
 % 
 % % Plot Bz on the volume
 % figure(3), hold on, box on, grid on
-% Gamma = winding_coordinates_rotated;
+ Gamma = winding_coordinates_rotated;
 % plot3(Gamma(:,1),Gamma(:,2),Gamma(:,3),'.-r') % plot filament
 % slice(X,Y,Z,BZ,[0],[],[-1,0,1]), colorbar % plot Bz
 % xlabel ('x [m]'), ylabel ('y [m]'), zlabel ('z [m]'), title ('Bz [T]')
@@ -155,14 +155,14 @@ xlabel ('x [m]'), ylabel ('y [m]'), title ('Bz [T]')
 % caxis([-0.5,0.5]*1e-5)
 % 
 % 
-% % Plot some flux tubes
-% 
-% figure(4), hold on, box on, grid on
-% plot3(Gamma(:,1),Gamma(:,2),Gamma(:,3),'.-r') % plot filament
-% [X0,Y0,Z0] = ndgrid(-1.5:0.5:1.5,-1.5:0.5:1.5,-2); % define tubes starting point
-% htubes = streamtube(stream3(X,Y,Z,BX,BY,BZ,X0,Y0,Z0), [0.2 10]);
-% xlabel ('x [m]'), ylabel ('y [m]'), zlabel ('z [m]'), title ('Some flux tubes')
-% view(3), axis equal, axis tight
-% set(htubes,'EdgeColor','none','FaceColor','c') % change tube color
-% camlight left % change tube light
+% Plot some flux tubes
+
+figure(4), hold on, box on, grid on
+plot3(Gamma(:,1),Gamma(:,2),Gamma(:,3),'.-r') % plot filament
+[X0,Y0,Z0] = ndgrid(-1.5:0.5:1.5,-1.5:0.5:1.5,-2); % define tubes starting point
+htubes = streamtube(stream3(X,Y,Z,BX,BY,BZ,X0,Y0,Z0), [0.2 10]);
+xlabel ('x [m]'), ylabel ('y [m]'), zlabel ('z [m]'), title ('Some flux tubes')
+view(3), axis equal, axis tight
+set(htubes,'EdgeColor','none','FaceColor','c') % change tube color
+camlight left % change tube light
 
